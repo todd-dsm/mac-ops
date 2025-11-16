60de595773f0c15c0d290a7202f8e9edb1d35ee2
Todd E Thomas <todd.dsm@gmail.com>
2025-11-16 13:45:25 -0800

    patched from main

--- ansible/roles/vim/files/vim.zsh
+++ ansible/roles/vim/files/vim.zsh
@@ -3,12 +3,14 @@
 # --------------------------------------------------------------------------- #
 # Set vim as default editor
 export EDITOR='vim'
-export VISUAL="$EDITOR"          # Double quotes needed for variable expansion
+export VISUAL="$EDITOR"
 
 # Alias vi to vim
 alias vi='vim'
 
 # Optional: Uncomment to use neovim as default editor
 # export EDITOR='nvim'
+# Alias vi to vim
 # export VISUAL="$EDITOR"
 # alias vi='nvim'
+alias ni='nvim'
